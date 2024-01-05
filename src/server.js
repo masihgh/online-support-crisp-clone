ffc0561df3cdf2569f73fa3dc12dddd5af36710e
masihgh <masihprogrammer@gmail.com>
2024-01-05 22:44:24 +0330

    simple app express

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+app.use(cors())
+app.get('/', (req, res) => {
+  req.send('Server is up and running')
+})
+http.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+})
